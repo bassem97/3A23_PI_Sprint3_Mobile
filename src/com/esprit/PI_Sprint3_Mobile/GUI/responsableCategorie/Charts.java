937c12a4a9ea6d33f455e5e8493766d5a8fd1cca
Zgon7 <karim.mannai7@gmail.com>
2021-05-21 13:38:52 +0100

    Chayma Stats

--- src/com/esprit/PI_Sprint3_Mobile/GUI/responsableCategorie/Charts.java
+++ src/com/esprit/PI_Sprint3_Mobile/GUI/responsableCategorie/Charts.java
@@ -6,17 +6,34 @@ import com.codename1.charts.renderers.DefaultRenderer;
 import com.codename1.charts.renderers.SimpleSeriesRenderer;
 import com.codename1.charts.util.ColorUtil;
 import com.codename1.charts.views.PieChart;
+import com.codename1.ui.FontImage;
 import com.codename1.ui.Form;
 import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.util.Resources;
+import com.esprit.PI_Sprint3_Mobile.entities.Categorie;
+import com.esprit.PI_Sprint3_Mobile.entities.ResponsableCategorie;
+import com.esprit.PI_Sprint3_Mobile.entities.Sujet;
+import com.esprit.PI_Sprint3_Mobile.entities.Theme;
+import com.esprit.PI_Sprint3_Mobile.services.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 public class Charts {
-    /**
-     * Creates a renderer for the specified colors.
-     */
+
+    public Charts() {
+        this.createPieChartForm().show();
+
+    }
+
+
+
     private DefaultRenderer buildCategoryRenderer(int[] colors) {
         DefaultRenderer renderer = new DefaultRenderer();
-        renderer.setLabelsTextSize(15);
-        renderer.setLegendTextSize(15);
+        renderer.setLabelsTextSize(50);
+        renderer.setLegendTextSize(50);
         renderer.setMargins(new int[]{20, 30, 15, 0});
         for (int color : colors) {
             SimpleSeriesRenderer r = new SimpleSeriesRenderer();
@@ -26,36 +43,39 @@ public class Charts {
         return renderer;
     }
 
-    /**
-     * Builds a category series using the provided values.
-     *
-     * @param title the series titles
-     * @param values the values
-     * @return the category series
-     */
-    protected CategorySeries buildCategoryDataset(String title, double[] values) {
-        CategorySeries series = new CategorySeries(title);
-        int k = 0;
-        for (double value : values) {
-            series.add("Project " + ++k, value);
-        }
 
+    protected CategorySeries buildCategoryDataset(String title, HashMap<String, Integer> values) {
+        CategorySeries series = new CategorySeries(title);
+
+        values.forEach(series::add);
         return series;
     }
 
     public Form createPieChartForm() {
-
         // Generate the values
-        double[] values = new double[]{12, 14, 11, 10, 19};
+
+        ArrayList<Categorie> categories = CategorieService.getInstance().findAll();
+        ArrayList<ResponsableCategorie> responsableCategories = ResponsableCategorieService.getInstance().findAll();
+        HashMap<String, Integer> values = new HashMap<>();
+        categories.forEach(c -> values.put(c.getTitre(), (int) responsableCategories.stream().filter(rc -> rc.getCategorie().getId() == c.getId()).count()));
+
 
         // Set up the renderer
-        int[] colors = new int[]{ColorUtil.BLUE, ColorUtil.GREEN, ColorUtil.MAGENTA, ColorUtil.YELLOW, ColorUtil.CYAN};
+        int[] colors = new int[20];
+        for (int i = 0; i < categories.size(); i++) {
+            Random r = new Random();
+            int low = ColorUtil.GREEN;
+            int high = ColorUtil.CYAN;
+            int result = r.nextInt(high-low) + low;
+            colors[i] = result;
+        }
         DefaultRenderer renderer = buildCategoryRenderer(colors);
         renderer.setZoomButtonsVisible(true);
         renderer.setZoomEnabled(true);
-        renderer.setChartTitleTextSize(20);
+        renderer.setChartTitleTextSize(30);
         renderer.setDisplayValues(true);
         renderer.setShowLabels(true);
+        renderer.setLabelsColor(ColorUtil.BLACK);
         SimpleSeriesRenderer r = renderer.getSeriesRendererAt(0);
         r.setGradientEnabled(true);
         r.setGradientStart(0, ColorUtil.BLUE);
@@ -63,15 +83,17 @@ public class Charts {
         r.setHighlighted(true);
 
         // Create the chart ... pass the values and renderer to the chart object.
-        PieChart chart = new PieChart(buildCategoryDataset("Project budget", values), renderer);
+        PieChart chart = new PieChart(buildCategoryDataset("Categorie", values), renderer);
 
         // Wrap the chart in a Component so we can add it to a form
         ChartComponent c = new ChartComponent(chart);
 
         // Create a form and show it.
-        Form f = new Form("Budget");
-        f.setLayout(new BorderLayout());
-        f.addComponent(BorderLayout.CENTER, c);
+        Form f = new Form(" Statistique", new BorderLayout());
+        f.add(BorderLayout.CENTER, c);
+        f.getToolbar().addCommandToLeftBar(null, FontImage.createMaterial(FontImage.MATERIAL_ARROW_BACK, "", 5), evt1 -> new ResponsableCategorieListForm().show());
         return f;
+
     }
+
 }
